443083ac977b182cc005f5c4a465cc647d046796
Margaret <match14@yandex.ru>
2021-09-08 22:05:01 +0300

    faq

--- src/js/index.js
+++ src/js/index.js
@@ -94,13 +94,6 @@ if($('.js-instructors-slider').length){
 					slidesToScroll: 1,
 				}
 			},
-		// 	{
-		// 		breakpoint: 768,
-		// 		settings: {
-		// 			centerPadding: '40px',
-		// 			slidesToShow: 1,
-		// 		}
-		// 	},
 		]
 	});
 }
@@ -125,6 +118,19 @@ if($('.js-open-menu').length){
 		$(this).toggleClass('active');
 		$(this).closest('.js-main-menu-item').children('.js-main-menu-sub').slideToggle(300);
 	});
+}
 
-	
+// unwrap block
+if($('.js-unwrap-block').length){
+	$('.js-unwrap-head').on('click',function(event){
+		event.preventDefault();
+		var $parent = $(this).parents('.js-unwrap-block');
+		
+		$parent.toggleClass('opened');
+		if($parent.hasClass('opened')){
+			$parent.children('.js-unwrap-content').slideDown(400);
+		}else{
+			$parent.children('.js-unwrap-content').slideUp(400);
+		}
+	});
 }
